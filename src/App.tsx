b9f7323f125f83e95a17938f3c0a164ebfd698c4
Samer Kayali <samerkkr@gmail.com>
2021-09-06 19:51:36 -0300

    feat: added types to and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Routes
+import { Home, Cart } from "./routes";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/cart" exact component={Cart} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
