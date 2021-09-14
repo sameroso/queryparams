b4824a0343b883a8743cf16b048af6b1601c5b37
Samer Kayali <samerkkr@gmail.com>
2021-09-13 22:12:06 -0300

    refactor: refactored docs

--- src/utils/functions/queryParams.ts
+++ src/utils/functions/queryParams.ts
@@ -1,52 +1,47 @@
-interface SpecificParams<T extends Record<string, string>> {
-  key: keyof T;
-  value: string;
-}
-
-interface GenericParams {
-  key: string;
-  value: string;
-}
-
-export function queryParamsFactory<T extends Record<string, string>>(
-  queryUrl: string
-) {
+export function urlSearchParamsFactory<
+  T extends Record<string, string> = Record<string, string>
+>(queryUrl: string) {
   const urlSearchParams = new URLSearchParams(queryUrl);
   return {
     urlSearchParams,
+
     /**
+     * @description                       [add single param to url search params]
      *
-     * @param key - the key of the param to add
-     * @param value - the value the param to add
-     * @returns query param Url string modified
+     * @param   {string}        key       [the key of the param to add]
+     * @param   {string}        value     [the value the param to add]
+     *
+     * @return  {string}                  [Url search params string modified]
+     *
      * @example
-     *  const { addParam } = queryParamsFactory('?key1=value1')
-     *  const urlWithAddedParam = addParam({ key:"key2", value:"value2"})
+     *  const { addParam } = urlSearchParamsFactory('?key1=value1')
+     *  const urlSearchParamsWithAddedParam = addParam({ key:"key2", value:"value2"})
      *
-     *  console.log(urlWithAddedParam) => "key1=value1&key2=value2"
+     *  console.log(urlSearchParamsWithAddedParam) => "key1=value1&key2=value2"
      *
      */
 
-    addParam({ key, value }: GenericParams) {
+    addParam({ key, value }: { key: string; value: string }): string {
       urlSearchParams.append(key, value);
       return urlSearchParams.toString();
     },
 
     /**
-     *@description
-     - add param list to declared query params Url
-     * @param key - the key of the param to add
-     * @param value - the value the param to add
-     * @returns query param Url string modified
+     * @description                          [add param list to url search params]
+     *
+     * @param   {string}         key         [the key of the param to add]
+     * @param   {string}         value       [the value the param to add]
+     *
+     * @return  {string}                     [Url search params string modified]
+     *
      * @example
-     *  const { addParamList } = queryParamsFactory('?key1=value1')
-     *  const urlWithAddedParamList = addParamList([{ key:"key2", value:"value2"},{ key:"key3", value:"value3"}])
+     *  const { addParamList } = urlSearchParamsFactory('?key1=value1')
+     *  const urlSearchParamsWithAddedParamList = addParamList([{ key:"key2", value:"value2"},{ key:"key3", value:"value3"}])
      *
-     *  console.log(urlWithAddedParamList) => "key1=value1&key2=value2&key3=value3"
-     *
+     *  console.log(urlSearchParamsWithAddedParamList) => "key1=value1&key2=value2&key3=value3"
      */
 
-    addParamList(paramsList: GenericParams[]) {
+    addParamList(paramsList: { key: string; value: string }[]): string {
       paramsList.forEach(({ key, value }) => {
         urlSearchParams.append(key as string, value);
       });
@@ -54,37 +49,40 @@ export function queryParamsFactory<T extends Record<string, string>>(
     },
 
     /**
-     * @description
-     * remove specific param
-     * @param key - param wanted to remove
-     * @returns query param Url string modified
-     * @example
-     *  const { removeParam } = queryParamsFactory('?key1=value1&key2=value2&key3=value3&key4=value4')
-     *  const urlWithRemovedParam = removeParam("key2")
+     * @description           [remove specific param]
      *
-     *  console.log(urlWithRemovedParam) => "key1=value1&key3=value3&key4=value4"
+     * @param   {string}  key  [param key wanted to remove]
+     *
+     * @return  {string}      [Url search params string modified]
+     *
+     * @example
+     *  const { removeParam } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3&key4=value4')
+     *  const urlSearchParamsWithRemovedParam = removeParam("key2")
+     *
+     *  console.log(urlSearchParamsWithRemovedParam) => "key1=value1&key3=value3&key4=value4"
      *
      */
 
-    removeParam(key: keyof T) {
+    removeParam(key: keyof T): string {
       urlSearchParams.delete(key as string);
       return urlSearchParams.toString();
     },
 
     /**
-     * @description
-     * remove params list specified by an array of strings representing the key
-     * @param key - Array of params wanted to remove
-     * @returns query param Url string modified
+     * @description                               [remove params list specified by an array of strings representing the key]
+     *
+     * @param   { key:string[] }    paramsList        [array of strings representing the keys wanted to remove]
+     *
+     * @return  {string}                        [Url search params string modified]
+     *
      * @example
-     *  const { removeParamList } = queryParamsFactory('?key1=value1&key2=value2&key3=value3&key4=value4')
-     *  const urlWithRemovedParamList = removeParamList(["key2", "key3"])
+     *  const { removeParamList } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3&key4=value4')
+     *  const urlSearchParamsWithRemovedParamList = removeParamList(["key2", "key3"])
      *
-     *  console.log(urlWithRemovedParamList) => "key1=value1&key4=value4"
-     *
+     *  console.log(urlSearchParamsWithRemovedParamList) => "key1=value1&key4=value4"
      */
 
-    removeParamList(paramsList: (keyof T)[]) {
+    removeParamList(paramsList: (keyof T)[]): string {
       paramsList.forEach((key) => {
         urlSearchParams.delete(key as string);
       });
@@ -92,39 +90,42 @@ export function queryParamsFactory<T extends Record<string, string>>(
     },
 
     /**
-     * @description
-     * if param exists it replaces the param, if it does not exists it adds param
+     * @description                                  [if param exists it replaces the param, if it does not exists it adds param]
      *
-     * @param key - param key wanted to to add or replace
-     * @param value - param value wanted to add or
-     * @returns query param Url string modified
+     * @param   {string}                      key    [param key wanted to to add or replace]
+     * @param   {string}                      value  [param value wanted to to add or replace]
+     *
+     * @return  {string}                             [Url search params string modified]
+     *
      * @example
-     *  const { addOrReplaceParam } = queryParamsFactory('?key1=value1&key2=value2')
-     *  const urlWithAddedAndReplaceParam = removeParam([{key:"key2", value:"replacedValue2"},{key:"key3", value:"addedValue3"}])
+     *  const { addOrReplaceParam } = urlSearchParamsFactory('?key1=value1&key2=value2')
+     *  const urlSearchParamsWithAddedOrReplacedParam = addOrReplaceParam({key:"key2", value:"replacedValue2"})
      *
-     *  console.log(urlWithAddedAndReplaceParam) => "key1=value1&key2=replacedValue2&key3=addedValue3"
+     *  console.log(urlSearchParamsWithAddedOrReplacedParam) => "key1=value1&key2=replacedValue2"
      */
 
-    addOrReplaceParam({ key, value }: SpecificParams<T>) {
+    addOrReplaceParam({ key, value }: { key: keyof T; value: string }): string {
       urlSearchParams.set(key as string, value);
       return urlSearchParams.toString();
     },
 
     /**
-     * @description
-     * if param exists it replaces the param, if it does not exists it adds param
+     * @description                                                [if param exists it replaces the param, if it does not exists it adds param]
      *
-     * @param key - param key wanted to to add or replace
-     * @param value - param value wanted to add or
-     * @returns query param Url string modified
+     * @param   {{ key:string; value:string }[]}       paramsList  [paramsList description]
+     *
+     * @return  {string}                                           [Url search params string modified]
+     *
      * @example
-     *  const { addOrReplaceParamList } = queryParamsFactory('?key1=value1&key2=value2')
-     *  const urlWithAddedAndReplaceParam = addOrReplaceParamList([{key:"key2", value:"replacedValue2"},{key:"key3", value:"addedValue3"}])
+     *  const { addOrReplaceParamList } = urlSearchParamsFactory('?key1=value1&key2=value2')
+     *  const urlSearchParamWithAddedAndReplacedParam = addOrReplaceParamList([{key:"key2", value:"replacedValue2"},{key:"key3", value:"addedValue3"}])
      *
-     *  console.log(urlWithAddedAndReplaceParam) => "key1=value1&key2=replacedValue2&key3=addedValue3"
+     *  console.log(urlSearchParamWithAddedAndReplacedParam) => "key1=value1&key2=replacedValue2&key3=addedValue3"
      */
 
-    addOrReplaceParamList(paramsList: SpecificParams<T>[]) {
+    addOrReplaceParamList(
+      paramsList: { key: keyof T; value: string }[]
+    ): string {
       paramsList.forEach(({ key, value }) => {
         urlSearchParams.set(key as string, value);
       });
@@ -132,98 +133,109 @@ export function queryParamsFactory<T extends Record<string, string>>(
     },
 
     /**
-     * @description
-     * get specific param on Url
+     * @description            [get specific param on url search params]
      *
-     * @param key - key of value wanted to get
-     * @returns param value
+     * @param   {string}  key  [key of value wanted to get]
+     *
+     * @return  {string}       [Url search params string modified]
+     *
      * @example
-     *  const { getParam } = queryParamsFactory('?key1=value1&key2=value2')
+     *  const { getParam } = urlSearchParamsFactory('?key1=value1&key2=value2')
      *  const param = getParam("key2")
      *
      *  console.log(param) => "value2"
+     *
      */
 
-    getParam(key: keyof T) {
+    getParam(key: keyof T): string | null {
       return urlSearchParams.get(key as string);
     },
 
     /**
-     * @description
-     * get param list on Url
+     * @description                              [get param list on url search params]
      *
-     * @param keys - Array of values wanted to get params
-     * @returns object containing param values
+     * @param   {string[]}            keys       [Array of values wanted to get params]
+     *
+     * @return  {string}                          [object containing param values]
      * @example
-     *  const { getParamList } = queryParamsFactory('?key1=value1&key2=value2&key3=value3')
+     *  const { getParamList } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3')
      *  const params = getParamList(["key2","key3"])
      *
      *  console.log(params) => { key2:"value2", key3:"value3"};
      *
      */
 
-    getParamList(keys: Array<keyof T>) {
+    getParamList(keys: Array<keyof T>): Record<keyof T, string | null> {
       return keys.reduce((acc, key) => {
         return { ...acc, [key]: urlSearchParams.get(key as string) };
-      }, {});
+      }, {}) as Record<keyof T, string | null>;
     },
 
     /**
-     * @description
-     * get all on Url
+     * @description                                                   [get all params on url search params]
      *
-     * @returns object containing all param values
-     * @example
-     *  const { getAllParams } = queryParamsFactory('?key1=value1&key2=value2&key3=value3')
+     *
+     * @return  {Record<string, string>}                              [object containing all param values]
+     *
+     *  @example
+     *  const { getAllParams } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3')
      *  const params = getAllParams()
      *
      *  console.log(params) => { key1:"value1", key2:"value2", key3:"value3" };
      */
 
-    getAllParams() {
+    getAllParams(): Record<keyof T, string> | Record<string, never> {
       const entries = urlSearchParams.entries();
       let entriesValues = entries.next();
-      let params: Record<string, string> = {
-        [entriesValues.value[0]]: entriesValues.value[1],
-      };
-      while (!entriesValues.done) {
-        params[entriesValues.value[0]] = entriesValues.value[1];
+
+      let params: Record<string, string> = !entriesValues.done
+        ? {
+            [entriesValues?.value?.[0]]: entriesValues.value?.[1],
+          }
+        : {};
+
+      while (!entriesValues.done && entriesValues.value) {
+        params[entriesValues?.value?.[0]] = entriesValues?.value?.[1];
         entriesValues = entries.next();
       }
-      return params as Record<keyof T, string>;
+
+      return params as Record<keyof T, string> | Record<string, never>;
     },
 
     /**
-     * @description
-     * get query param Url
+     * @description        [get Url search params]
      *
-     * @returns object containing all param values
+     * @return  {string}  [url search params]
+     *
      * @example
-     *  const { getQueryParamUrl } = queryParamsFactory('?key1=value1&key2=value2&key3=value3')
+     *  const { getQueryParamUrl } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3')
      *  const queryParamUrl = getQueryParamUrl()
      *
      *  console.log(queryParamUrl) => "key1=value1&key2=value2&key3=value3";
      */
 
-    getQueryParamUrl() {
+    getUrlSearchParams(): string {
       return urlSearchParams.toString();
     },
+
     /**
-     * @description
-     * compose can be used to execute more than one function and returns the result
+     * @description                          [compose can be used to execute more than one function and returns the result url search params modified]
      *
-     * @returns query param Url string modified
+     * @param   {Array<Function>}  fns       [functions selected to compose and modify url search params]
+     *
+     * @return  {string}            [Url search params string modified]
+     *
      * @example
-     *  const { compose, addOrReplaceParamList, removeParamList } = queryParamsFactory('?key1=value1&key2=value2&key3=value3&key5=value5')
-     *  const modifiedUrl = compose(
+     *  const { compose, addOrReplaceParamList, removeParamList } = urlSearchParamsFactory('?key1=value1&key2=value2&key3=value3&key5=value5')
+     *  const modifiedUrlSearchParams = compose(
      *  () => addOrReplaceParamList([{key:"key1", value:"replacedValue1"},{key:"key4", value:"value4"}]),
      *  () => removeParamList(["key2", "key5"])
      * )
      *
-     *  console.log(modifiedUrl) => "?key1=replacedValue1&key3=value3&key4=value4"
+     *  console.log(modifiedUrlSearchParams) => "?key1=replacedValue1&key3=value3&key4=value4"
      */
 
-    compose(...fns: Array<Function>) {
+    compose(...fns: Array<Function>): string {
       fns.forEach((fn) => {
         fn();
       });
